f76a18cf41957adb823b71c335c4410f52beb59d
Hakaba Hitoyo <hakabahitoyo@example.com>
2017-07-13 13:53:25 +0900

    switch boilerplate

--- client/user-speed.js
+++ client/user-speed.js
@@ -18,6 +18,7 @@ window.addEventListener ('load', function () {
 
 
 function show_users (users) {
+var boilerplate = (window.location.search === '?boilerplate');
 var placeholder = document.getElementById ('placeholder-users');
 var html = '';
 var cn;
@@ -36,8 +37,7 @@ for (cn = 0; cn < users.length; cn ++) {
 		(user.speed * 60 * 60 * 24).toFixed (1) + ' トゥート/日 (' + (user.speed_order + 1) + ' 位)' + '&emsp;' +
 		'手動得点 ' + score_s + '&emsp;' +
 		'総合順位 ' + (user.recommendation_order + 1) + ' 位' +
-		'<br>' +
-		'<small>' + getTemplate (user.host, user.username, score_s) + '</small>' +
+		(boilerplate? '<br>' + '<small>' + getTemplate (user.host, user.username, score_s) + '</small>': '') +
 		'</p>';
 	html += user_html;
 }
